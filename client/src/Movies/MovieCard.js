bd4ad5be5c6361850f79aa21a3b505d030c8733d
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-11 17:17:38 -0500

    added MovieCard component

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React from 'react'
+import { useHistory } from 'react-router-dom'
 
-export default function MovieCard (props) {
-  return;
+export default function MovieCard(props) {
+  const { id, title, director, metascore, stars = null } = props.movie
+
+  const history = useHistory()
+
+  const routeToMovie = (id) => {
+    if (!stars) history.push(`/movies/${id}`)
+  }
+
+  return (
+    <div className='save-wrapper'>
+      <div className='movie-card' onClick={() => routeToMovie(id)}>
+        <h2>{title}</h2>
+        <div className='movie-director'>
+          Director: <em>{director}</em>
+        </div>
+        <div className='movie-metascore'>
+          Metascore: <strong>{metascore}</strong>
+        </div>
+
+        {stars &&
+          stars.map((star) => (
+            <div key={star} className='movie-star'>
+              {star}
+            </div>
+          ))}
+      </div>
+      {stars && <div className='save-button'>Save</div>}
+    </div>
+  )
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,19 +1,8 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import MovieCard from './MovieCard'
 
 export default function MovieList(props) {
-  return (
-    <div className='movie-list'>
-      {props.movies.map((movie) => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  )
-}
-
-function MovieDetails(props) {
-  const { title, director, metascore, id } = props.movie
-
   const history = useHistory()
 
   const routeToMovie = (id) => {
@@ -21,14 +10,12 @@ function MovieDetails(props) {
   }
 
   return (
-    <div className='movie-card' onClick={() => routeToMovie(id)}>
-      <h2>{title}</h2>
-      <div className='movie-director'>
-        Director: <em>{director}</em>
-      </div>
-      <div className='movie-metascore'>
-        Metascore: <strong>{metascore}</strong>
-      </div>
+    <div className='movie-list'>
+      {props.movies.map((movie) => (
+        <div key={movie.id} onClick={() => routeToMovie(movie.id)}>
+          <MovieCard movie={movie} />
+        </div>
+      ))}
     </div>
   )
 }
